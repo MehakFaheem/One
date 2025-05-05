586b3a50790553d5574e1191cc34ae19946f27db
mehak-47 <iamfamous073@gmail.com>
2025-05-05 21:10:59 +0500

    Disable ESLint during build for Vercel

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  ignoreDuringBuilds: true,
-  // experimental: {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },  // experimental: {
   //   esmExternals: false,
   // },
 };
